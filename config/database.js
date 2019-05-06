acdf3b5429cf55b0aa86fd98edcba9cba3600c89
Sridharan Murugadass <sridharanbe.ece@gmail.com>
2019-05-06 11:55:51 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,3 +2,5 @@ module.exports = {
     remoteUrl : 'mongodb://test:test123@ds123312.mlab.com:23312/menulist',
     localUrl: 'mongodb://test:test123@ds123312.mlab.com:23312/menulist'
 };
+
+//https://help.grapecity.com/spread/SpreadSheets12/webframe.html#spdesigner.html
